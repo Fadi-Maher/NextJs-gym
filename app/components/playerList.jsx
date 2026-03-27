d71f04e1c9221e98d3cba8be1157ff6b355e40b9
Fady Maher <cleverfady@gmail.com>
2026-03-27 17:17:08 +0200

    handle cors

--- app/components/playerList.jsx
+++ app/components/playerList.jsx
@@ -137,7 +137,7 @@ export default function PlayersList({ selectedPlayers, setSelectedPlayers }) {
             </p>
           </div>
         </div>
-
+{""}
         <div className="flex flex-wrap gap-3">
           <div className="relative">
             <Search className="absolute left-3 top-1/2 transform -translate-y-1/2 w-5 h-5 text-gray-400" />
